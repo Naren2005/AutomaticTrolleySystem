b7984f2c09951f329f012d58616a4afbd87cfa78
Naren2005 <narenmedarametla23@gmail.com>
2023-12-03 16:39:11 +0530

    Billing System complete

--- Database.py
+++ Database.py
@@ -1,6 +1,6 @@
 from twilio.rest import Client
 account_sid = "ACa28af891c27eceb77f6c9305511d42eb"
-auth_token = "63e552553cc19ec4eb2826ce95f65882"
+auth_token = "43784ce5d4ee616ecd5684545d4b547f"
 
 client = Client(account_sid,auth_token)
 
@@ -8,7 +8,7 @@ def send_sms(smz):
     client.messages.create(
             to="+919490932710",
             from_="+14439633415",
-            body= smz
+            body=smz
         )
 
 
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import cv2
+import numpy as np
 from pyzbar.pyzbar import decode
 from pyzbar.pyzbar import ZBarSymbol
 import serial
 from collections import Counter
 import pandas as pd
 import warnings
-from Database import send_sms
 import colorama
+
 
 warnings.filterwarnings("ignore",category=UserWarning)
 
 mydict = {}
 
-SerialComm = serial.Serial('COM6', baudrate=9600, timeout=0.5)
+SerialComm = serial.Serial('COM5', baudrate=9600, timeout=0.5)
 total_price = 0
 Item_list = []
 
@@ -28,10 +29,15 @@ def BarCodeDetector():
 
     while True:
         ret, frame = cap.read() # frame is a numpy array
-        decoded_objects = decode(frame,symbols=[ZBarSymbol.QRCODE])  # List of decoded obj, deletes after detected instantly
+        decoded_objects = decode(frame)  # List of decoded obj, deletes after detected instantly
 
         for obj in decoded_objects:
             objectData = obj.data.decode('utf-8')
+            pts = np.array([obj.polygon],np.int32)
+            pts = pts.reshape((-1,1,2))
+            cv2.polylines(frame,[pts],True,(255,0,255),5)
+            pts2 = obj.rect
+            cv2.putText(frame,objectData,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX,0.9,(255,0,255),2)
             Temp_list.append(objectData)
             if objectData not in Item_list:
                 if check_frequency(objectData,Temp_list) == True:
@@ -43,6 +49,7 @@ def BarCodeDetector():
             else:
                 if check_frequency(objectData,Temp_list) == True:
                     send_data_to_arduino("3")
+                    send_data_to_arduino("4")
                     Item_list.remove(objectData)
                     serial_checker(objectData,False)
                     Temp_list.clear()
@@ -50,7 +57,6 @@ def BarCodeDetector():
         cv2.imshow("Bar Code Scanner", frame)
         if cv2.waitKey(1) & 0xFF == 27:
             break
-    print(Item_list)
 
 
     cap.release()
@@ -73,7 +79,7 @@ def check_frequency(item_to_check, input_list):
     frequency_counter = Counter(input_list)
 
     # Check if the frequency of the specified item is above 20
-    if frequency_counter[item_to_check] > 50:
+    if frequency_counter[item_to_check] > 40:
         return True
 
 def serial_checker(serialcode,condition):
@@ -84,6 +90,8 @@ def serial_checker(serialcode,condition):
 
     # Your 6-digit number
     barcode = serialcode
+    barcode = barcode.strip()
+    barcode = str(barcode)
 
     # Check if the number exists in the 'Number' column
     if barcode in df['S.No'].values:
@@ -102,19 +110,35 @@ def serial_checker(serialcode,condition):
             k = f"+Added {item_name} : {price} Rs         Total: {total_price}"
             print(colorama.Fore.GREEN + k)
             print("")
-            send_data_to_arduino(k)
         elif condition == False:
             mydict.pop(item_name)
             total_price -= price
             t = f"-Removed {item_name} : {price} Rs         Total: {total_price}"
-            send_data_to_arduino(t)
             print(colorama.Fore.RED + t)
             print("")
 
     else:
-        print(f"This code: {barcode} does not exist in our Datasheet.")
+        print(f"{colorama.Fore.LIGHTMAGENTA_EX}This code: {barcode} does not exist in our Datasheet.")
+        print(barcode)
+
+from twilio.rest import Client
+account_sid = "ACa28af891c27eceb77f6c9305511d42eb"
+auth_token = "43784ce5d4ee616ecd5684545d4b547f"
+
+client = Client(account_sid,auth_token)
+
+def send_sms(smz):
+    client.messages.create(
+            to="+919490932710",
+            from_="+14439633415",
+            body=smz
+        )
+
 
 main()
 print(colorama.Fore.BLACK + "------Bill------" +  '\n')
+p = " "
 for key , values in mydict.items():
-    print(f"{colorama.Fore.RESET} {key} : Rs {values}")
+    print(f"{colorama.Fore.RESET} {key} : Rs {values} \n")
+    p += f"{key} : Rs {values}"
+send_sms(p)
